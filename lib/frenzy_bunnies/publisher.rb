3aa9f776ea00aaa3984db4daf97ce5ce41b0a436
Ernest Bursa <ernest.bursa@allegro.pl>
2014-09-17 22:39:45 +0200

    fix

--- lib/frenzy_bunnies/publisher.rb
+++ lib/frenzy_bunnies/publisher.rb
@@ -6,20 +6,23 @@ module FrenzyBunnies
     def initialize(connection, opts = {})
       @connection = connection
       @opts       = opts
+      @mutex      = Mutex.new
     end
 
     # publish(data, :routing_key => "resize")
     def publish_to_exchange(msg, exchange_name, routing={})
-      ch       = @connection.create_channel
-      exchange = MarchHare::Exchange.new(ch, exchange_name, symbolize(@opts[:exchanges][exchange_name]))
-
       if @connection.open?
-        exchange.publish(msg, routing_key: routing[:routing_key], properties: { persistent: @opts[:message_persistent] })
+        begin
+          # Synchronization required due to creating channel number in safe manner
+          channel = @mutex.synchronize { @connection.create_channel }
+          exchange = MarchHare::Exchange.new(channel, exchange_name, symbolize(@opts[:exchanges][exchange_name]))
+          exchange.publish(msg, routing_key: routing[:routing_key], properties: { persistent: @opts[:message_persistent] })
+        ensure
+          channel.close
+        end
       else
         raise Exception, 'Could not publish message, connection is closed!'
       end
-    ensure
-      ch.close
     end
 
   end
