845155465a316c7d7b3c5603c22b6061fae38709
Ernest Bursa <ernest.bursa@allegro.pl>
2014-09-18 16:25:06 +0200

    upgrade size

--- lib/frenzy_bunnies/publisher.rb
+++ lib/frenzy_bunnies/publisher.rb
@@ -10,7 +10,7 @@ module FrenzyBunnies
       @opts       = opts
       @persistent = @opts[:message_persistent]
 
-      @channel_pool = ConnectionPool.new(size: 50, timeout: 1) do
+      @channel_pool = ConnectionPool.new(size: 100, timeout: 0.1) do
         @connection.create_channel
       end
     end
