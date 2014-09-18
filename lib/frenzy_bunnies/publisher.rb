32bda63bafee1b5ed8e1da165566e35bba89b452
Ernest Bursa <ernest.bursa@allegro.pl>
2014-09-18 17:08:19 +0200

    upgrade size

--- lib/frenzy_bunnies/publisher.rb
+++ lib/frenzy_bunnies/publisher.rb
@@ -10,30 +10,30 @@ module FrenzyBunnies
       @opts       = opts
       @persistent = @opts[:message_persistent]
 
-      @channel_pool = ConnectionPool.new(size: 100, timeout: 0.1) do
-        @connection.create_channel
-      end
+      # @channel_pool = ConnectionPool.new(size: 100, timeout: 0.1) do
+      #   @connection.create_channel
+      # end
     end
 
     # publish(data, :routing_key => "resize")
     def publish_to_exchange(msg, exchange_name, routing = {})
       if @connection.open?
-        @channel_pool.with do |channel|
-          # channel = @connection.create_channel
+        # @channel_pool.with do |channel|
+          channel = @connection.create_channel
           # Synchronization required due to creating channel number in safe manner
           exchange = channel.exchange(exchange_name, symbolize(@opts[:exchanges][exchange_name]))
           exchange.publish(msg, routing_key: routing[:routing_key], properties: { persistent: @persistent })
-          # publishing_channel.close
-        end
+          channel.close
+        # end
       else
         raise Exception, 'Could not publish message, connection is closed!'
       end
     end
 
     def shutdown_connection_pool!
-      @channel_pool.shutdown do |connection|
-        connection.close
-      end
+      # @channel_pool.shutdown do |connection|
+      #   connection.close
+      # end
     end
 
   end
